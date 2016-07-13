c6bff1dd70b8826c4fc906b46cc930f98b434db5
Shokouh Farvid <farvid.sh@gmail.com>
2016-07-13 11:31:00 -0700

    get car information and display

--- Moduls/HomeModule.cs
+++ Moduls/HomeModule.cs
@@ -8,14 +8,17 @@ namespace CarDealership
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["add_car_info.cshtml"];
+      Get["/"] = _ => {
+        return View["add_car_info.cshtml"];
+      };
       Get["/view_all_cars"] = _ => {
         Car newCar = new Car(
-          Request.Query["new-car"],
+          Request.Query["new-model"],
           Request.Query["new-price"],
           Request.Query["new-miles"]
-          );
+        );
         return View["view_all_cars.cshtml", newCar];
+      };
     }
   }
 }
